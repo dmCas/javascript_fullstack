edbc42366f656064db5b27f95aa539ee1f2edf61
beichenloveNacy <857560476@qq.com>
2019-10-17 21:44:41 +0800

    js实现小鸟飞行

--- JS_study/JS-Fly-bird/js/index.js
+++ JS_study/JS-Fly-bird/js/index.js
@@ -1,10 +1,12 @@
 var jsWrapBg = document.querySelector('.wrapBg');
 var jsheaderTitle = document.querySelector('.header-title');
 var jsheaderBird = document.getElementById('header_bird')
-
+var blocksArr = []
+var blockDistance = baseObj.randomNum(120, 350)
 var Y =3
 var index =0
 var imgArr = ["./img/bird0.png", "./img/bird1.png"]
+
 
 var headWaveTimer = setInterval(headWave, 200)
 
@@ -29,10 +31,37 @@ function headWave(){
     if(jsGrassLand2.offsetLeft <=  -343){
       jsGrassLand2.style.left="343px"
     }
+
+    if(blocksArr.length) {
+      for(var i =0; i<blocksArr.length; i++){
+        blocksArr[i].moveBlock()
+        var x = baseObj.rectangleCrashExamine(blocksArr[i].downDivWrap,bird.div)
+        var y = baseObj.rectangleCrashExamine(blocksArr[i].upDivWrap,bird.div)
+        var z = bird.div.offsetTop >= 390
+        if( x || y || z){
+          bird.fallSpeed = 0
+          clearInterval(landTimer)
+          jsWrapBg.onclick = null
+        }
+      }
+      if(blocksArr[blocksArr.length-1].downDivWrap.offsetLeft < (450 - blockDistance)){
+        blockDistance = baseObj.randomNum(130, 250)
+        var newBlock = new Block()
+        newBlock.createBlock()
+        blocksArr.push(newBlock)
+      }
+      //移除多余的Dom节点 清理内存
+      if(blocksArr[0].downDivWrap.offsetLeft < -50){
+        jsWrapBg.removeChild(blocksArr[0].downDivWrap)
+        jsWrapBg.removeChild(blocksArr[0].upDivWrap)
+        blocksArr.shift(blocksArr[0])
+      }
+    }
     jsGrassLand1.style.left = jsGrassLand1.offsetLeft - 3 + "px"
     jsGrassLand2.style.left = jsGrassLand2.offsetLeft - 3 + "px"
   }
 
+ 
   var jsStartBtn = document.getElementById('startBtn')
   jsStartBtn.onclick = function() {
     jsheaderTitle.style.display = "none"
@@ -45,5 +74,10 @@ function headWave(){
     jsWrapBg.onclick = function(){
       bird.fallSpeed = -8
     }
-    bird.wingWave()
+    bird.wingWave() //煽动翅膀
+
+    var b = new Block()
+    b.createBlock()
+    // b.moveBlock()
+    blocksArr.push(b)
   }
