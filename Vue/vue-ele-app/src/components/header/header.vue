ed816a5f7d085e2d78abf72c05386a819ea31c66
beichenloveNacy <857560476@qq.com>
2019-11-01 09:36:05 +0800

    vue-first-project

---
+++ Vue/vue-ele-app/src/components/header/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <!-- 头部 -->
+    <div class="header">
+      这是头部内容
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {  
+}
+</script>
+
+<style scoped>
+</style>
