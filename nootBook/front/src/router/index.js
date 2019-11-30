5eefc7d97affb7168208bd175ac9791090b9bd37
beichenloveNacy <857560476@qq.com>
2019-11-30 13:59:43 +0800

    vue-node-project

---
+++ nootBook/front/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import StarNotes from '@/components/StarNotes'
+import StarBanner from '@/components/starBanner/StarBanner'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'StarNotes',
+      component: StarNotes,
+      //配置标签页
+      meta: {
+        title: '星辰笔记'
+      }
+    },
+    {
+      path: '/StarBanner',
+      name: 'StarBanner',
+      component: StarBanner,
+      meta: {
+        title: '欢迎'
+      } 
+    }
+
+  ]
+})
